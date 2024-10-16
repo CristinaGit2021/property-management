8f9167d3de746181a6ed94e308b70eaed6e39dc3
CristinaGit2021 <cristina.lazar@ibm.com>
2024-10-16 20:31:04 +0300

    Implemented error handling

--- src/main/java/com/mycompany/property_management/service/impl/UserServiceImpl.java
+++ src/main/java/com/mycompany/property_management/service/impl/UserServiceImpl.java
@@ -3,10 +3,16 @@ package com.mycompany.property_management.service.impl;
 import com.mycompany.property_management.converter.UserConverter;
 import com.mycompany.property_management.dto.UserDTO;
 import com.mycompany.property_management.entity.UserEntity;
+import com.mycompany.property_management.exception.BusinessException;
+import com.mycompany.property_management.exception.ErrorModel;
 import com.mycompany.property_management.repository.UserRepository;
 import com.mycompany.property_management.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -18,12 +24,37 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDTO register(UserDTO userDTO) {
-        UserEntity user = userRepository.save(userConverter.convertDtoToEntity(userDTO));
+        UserEntity user= null;
+       Optional<UserEntity> optionalUserEntity = userRepository.findByOwnerEmail(userDTO.getOwnerEmail());
+       if (optionalUserEntity.isPresent()) {
+           List<ErrorModel> errorModelList = new ArrayList<>();
+           ErrorModel errorModel = new ErrorModel();
+           errorModel.setCode("EMAIL ALREADY EXIST");
+           errorModel.setMessage("The email is already registered!");
+           errorModelList.add(errorModel);
+
+           throw new BusinessException(errorModelList);
+       }
+         user = userRepository.save(userConverter.convertDtoToEntity(userDTO));
         return userConverter.convertEntityToDto(user);
     }
 
     @Override
     public UserDTO login(String email, String password) {
-        return null;
+        UserDTO userDTO = null;
+        Optional<UserEntity> optionalUserEntity =  userRepository.findByOwnerEmailAndPassword(email, password);
+        if (optionalUserEntity.isPresent()) {
+            userDTO = userConverter.convertEntityToDto(optionalUserEntity.get());
+        } else {
+            List<ErrorModel> errorModelList = new ArrayList<>();
+            ErrorModel errorModel = new ErrorModel();
+            errorModel.setCode("INAVALID LOGIN");
+            errorModel.setMessage("Incorrect user email or password");
+            errorModelList.add(errorModel);
+
+            throw new BusinessException(errorModelList);
+        }
+
+        return userDTO;
     }
 }
